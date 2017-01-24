893e2cb6b11bfb417b7c84dbc42098e005d1b1f8
PrimaAnanda-PC <PrimaAnanda-PC@PrimaAnanda-PC>
2017-01-24 11:46:46 +0700

    done

---
+++ src/TP_6/Jurnal_2/KelasMK.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TP_6.Jurnal_2;
+
+/**
+ *
+ * @author PrimaAnanda-PC
+ */
+public class KelasMK {
+    
+}
